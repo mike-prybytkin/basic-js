dae0da89159d2cb7e1b66ea350ccd18f14b4a9b2
@Mike-Prybytkin <prybytkin@bk.ru>
2022-04-28 01:59:55 +0300

    feat: complete count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  const ears = '^^';
+  let arrFlat = matrix.flat();
+  let counter = 0;
+
+  arrFlat.forEach((el) => {
+    if(el === ears) {
+      counter++;
+    }
+  });
+
+  return counter;
 }
 
 module.exports = {
