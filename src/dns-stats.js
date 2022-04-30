a84e14491367b31936c7351d0d2f8e630948fd49
@Mike-Prybytkin <prybytkin@bk.ru>
2022-04-30 23:25:00 +0300

    feat: dns-stats task is implement

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,10 +22,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = {};
+  let revertDomain = [];
+  for (let i = 0; i < domains.length; i++) {
+      let revert = '';
+      
+      for (let j = 0, k = 0; j < domains[i].length; j++) {
+          if (domains[i][j]  === '.') {
+              revert = `${domains[i].slice(k, j)}` + `.${revert}` ;
+              k = j;
+          }
+
+          if ( j === domains[i].length - 1) {
+              if (revert[0] === '.') {
+                  revert = `${domains[i].slice(k, domains[i].length)}`  + `${revert}`;
+              } else {
+                  revert = `${domains[i].slice(k, domains[i].length)}.`  + `${revert}`;
+              }
+          }
+      }
+      revertDomain.push(revert);    
+  }
+
+  for (let i = 0; i < revertDomain.length; i++) {
+
+      for (let j = 0, k = 0; j < revertDomain[i].length; j++) {
+          let curr = '';
+          if (revertDomain[i][j]  === '.') {
+              curr = `${revertDomain[i].slice(k, j)}`;
+
+              if (curr in result) {
+                  result[curr] ++;
+              } else {
+                  result[curr] = 1;
+              }
+
+          }
+      }
+  }
+  delete result[''];
+  return result;
 }
+
+
 
 module.exports = {
   getDNSStats
