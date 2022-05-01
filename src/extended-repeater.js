7f8548c2433ac1c3102b76dc740e527230d1e9ab
@Mike-Prybytkin <prybytkin@bk.ru>
2022-05-01 18:52:00 +0300

    feat: extended-repeater task is implement

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(string, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let str = string + '';
+  let repeatTimes = 1;
+  let separator = '+';
+  let addition = '';
+  let additionRepeatTimes = 1;
+  let additionSeparator = '|';
+  
+  if ('repeatTimes' in options) {
+    repeatTimes = options['repeatTimes'];
+  }
+  if ('separator' in options) {
+    separator = options['separator'];
+  }
+  if ('addition' in options) {
+    addition = options['addition'] + '';
+  }
+  if ('additionRepeatTimes' in options) {
+    additionRepeatTimes = options['additionRepeatTimes'];
+  }
+  if ('additionSeparator' in options) {
+    additionSeparator = options['additionSeparator'];
+  }
+
+  let resultStr = '';
+  let subStr = '';
+
+  for (let i = 0; i < repeatTimes; i++) {
+    
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      subStr += `${addition}${additionSeparator}`;
+    }
+    resultStr += `${str}${subStr.slice(-subStr.length, -additionSeparator.length)}${separator}`
+    subStr = '';
+  }
+
+  return resultStr.slice(-resultStr.length, -separator.length);
 }
 
 module.exports = {
